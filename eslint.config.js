c7a23823f13027dfb90281651206564ebd6f9e5c
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2024-05-23 18:03:48 +0200

    Fix not working in dev db local

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,9 @@ import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  antfu({
-    unocss: true,
-    formatters: true,
-    files: ['**/*.ts', '**/*.vue', '**/*.md'],
-  }),
+  // antfu({
+  //   unocss: true,
+  //   formatters: true,
+  //   files: ['**/*.ts', '**/*.vue', '**/*.md'],
+  // }),
 )
