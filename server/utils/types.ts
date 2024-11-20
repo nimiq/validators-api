7f16d68e73d8550636456c1d99b33602867f213c
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2024-11-20 09:06:28 -0600

    fix: donut

--- server/utils/types.ts
+++ server/utils/types.ts
@@ -1,5 +1,5 @@
 import type { Range } from '~~/packages/nimiq-validators-trustscore/src'
-import type { Score, Validator } from './drizzle'
+import type { Activity, Score, Validator } from './drizzle'
 
 export type Result<T> = Promise<{ data: T, error: undefined } | { data: undefined, error: string }>
 
@@ -12,6 +12,7 @@ export enum PayoutType {
 export type ValidatorScore =
   Omit<Validator, 'hasDefaultLogo' | 'accentColor' | 'contact'>
   & Pick<Score, 'availability' | 'dominance' | 'reliability' | 'total'>
+  & Pick<Activity, 'dominanceRatioViaBalance' | 'dominanceRatioViaSlots'>
 
 export enum HealthFlag {
   MissingEpochs = 'missing-epochs',
