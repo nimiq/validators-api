eb15c272da1204e7cb50f330f2c6d61102c19c3d
onmax <maximogarciamtnez@gmail.com>
2024-11-20 18:45:03 -0600

    feat: update identicons-esm to version 0.2.0 and refactor logo handling logic

--- server/utils/logo.ts
+++ server/utils/logo.ts
@@ -1,7 +1,8 @@
 import type { ValidatorJSON } from './schemas'
 import { Buffer } from 'node:buffer'
 import { consola } from 'consola'
-import { createIdenticon, getIdenticonsParams } from 'identicons-esm'
+import { createIdenticon } from 'identicons-esm'
+import { getIdenticonsParams } from 'identicons-esm/core'
 import { optimize } from 'svgo'
 
 async function getDefaultBranding(address: string) {
@@ -15,14 +16,14 @@ async function getDefaultBranding(address: string) {
 export async function handleValidatorLogo(address: string, { logo: _logo, accentColor }: ValidatorJSON) {
   if (!_logo) {
     const params = await getDefaultBranding(address)
-    const path = await uploadLogo(address, params.logo)
-    return { ...params, logo: path }
+    // const path = await uploadLogo(address, params.logo)
+    return { ...params }
   }
 
   if (!accentColor)
     throw new Error(`The validator ${address} does have an logo but not an accent color`)
 
-  let logo: string = _logo
+  const logo: string = _logo
 
   if (logo.startsWith('data:image/svg+xml')) {
     // Handle both base64 and URL-encoded SVGs
@@ -68,29 +69,61 @@ export async function handleValidatorLogo(address: string, { logo: _logo, accent
       consola.warn(`SVGO optimization failed for ${address}`)
     }
 
-    logo = `data:image/svg+xml,${encodeURIComponent(optimizedSvg)}`
+    // logo = `data:image/svg+xml,${encodeURIComponent(optimizedSvg)}`
   }
 
-  const pathname = await uploadLogo(address, logo)
-  return { logo: pathname, accentColor: accentColor!, hasDefaultLogo: false }
+  // const pathname = await uploadLogo(address, logo)
+  return { logo, accentColor: accentColor!, hasDefaultLogo: false }
 }
 
-async function uploadLogo(address: string, logo: string) {
-  const [header, base64Data] = logo.split(',')
-  const mime = header.split(':')[1].split(';')[0]
-  const binaryData = Buffer.from(base64Data, 'base64')
-  const blob = new Blob([binaryData], { type: mime })
-  ensureBlob(blob, { maxSize: '1024KB', types: ['image'] })
+// async function uploadLogo(address: string, logo: string) {
+//   const { blob, fileExt, mime } = getBlob(logo)
+//   ensureBlob(blob, { maxSize: '1024KB', types: ['image'] })
+//   consola.info(`Uploading logo for ${blob.size} bytes for ${address}`)
+//   const pathname = `validator/${address.replaceAll(' ', '-')}.${fileExt}`
+//   const res = await hubBlob().put(pathname, blob, { contentType: mime })
 
-  const fileExt = mime.split('/')[1].replace('svg+xml', 'svg')
-  const pathname = `validator/${address.replaceAll(' ', '-')}.${fileExt}`
-  const res = await hubBlob().put(pathname, blob)
+//   const exists = await hubBlob().head(pathname)
+//   if (!exists)
+//     throw new Error(`Error uploading logo for ${address}`)
 
-  const exists = await hubBlob().head(pathname)
-  if (!exists)
-    throw new Error(`Error uploading logo for ${address}`)
+//   if (!res.size)
+//     throw new Error(`Error uploading logo for ${address}`)
+//   return `/images/${pathname}`
+// }
 
-  if (!res.size)
-    throw new Error(`Error uploading logo for ${address}`)
-  return `/images/${pathname}`
-}
+// function getBlob(logo: string): { blob: Blob, mime: string, fileExt: string } {
+//   const isDataUri = logo.startsWith('data:')
+//   if (!isDataUri)
+//     throw new Error('Invalid Data URI input')
+
+//   const [header, data] = logo.split(',')
+//   const mime = header.split(':')[1].split(';')[0]
+//   const fileExt = mime.split('/')[1].replace('svg+xml', 'svg')
+
+//   consola.info(`Converting logo with mime type ${mime} to blob`)
+//   const blob = b64toBlob(data, mime, header.includes('base64'))
+//   return { blob, mime, fileExt }
+// }
+
+// function b64toBlob(data: string, contentType = '', isBase64: boolean) {
+//   try {
+//     const byteCharacters = isBase64 ? atob(data) : decodeURIComponent(data)
+//     const byteArrays = []
+
+//     for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+//       const slice = byteCharacters.slice(offset, offset + 512)
+//       const byteNumbers: number[] = Array.from({ length: slice.length })
+//       for (let i = 0; i < slice.length; i++) {
+//         byteNumbers[i] = slice.charCodeAt(i)
+//       }
+//       const byteArray = new Uint8Array(byteNumbers)
+//       byteArrays.push(byteArray)
+//     }
+
+//     return new Blob(byteArrays, { type: contentType })
+//   }
+//   catch (error) {
+//     throw new Error(`Invalid base64 data: ${error.message}`)
+//   }
+// }
